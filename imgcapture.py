198d7879eabc5e6633fdaf16ebba987c5de9728e
P-Jayaprakash <68218345+P-Jayaprakash@users.noreply.github.com>
2020-09-12 20:27:08 +0530

    Add files via upload

---
+++ imgcapture.py
@@ -0,0 +1,9 @@
+import cv2
+imgcapture=cv2.VideoCapture(0)
+result=True
+while(result):
+    ret, frame=imgcapture.read()
+    cv2.imwrite("test.jpg",frame)
+    result=False
+    print("image captured....!")
+imgcapture.release()    
